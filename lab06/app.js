6fac927930de22502524d9ec2731fca3b65085fe
ShweYM <38621520+ShweYM@users.noreply.github.com>
2020-09-10 10:16:04 +0800

    update lab06

--- lab06/app.js
+++ lab06/app.js
@@ -3,15 +3,14 @@ const path = require("path");
 const qr = require("qr-image");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+const bodyParser = require("body-parser");
 
 const app = express();
 // app.use(express.static(path.join(__dirname, "qr")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("QR");
-});
-
-const getRespType = (type) => {
+const getRespType = (options) => {
   if (options.type == "svg") {
     return "svg";
   } else if (options.type == "pdf") {
@@ -20,16 +19,59 @@ const getRespType = (type) => {
   return "image/png";
 };
 
-app.get("/qr-async/:data", async (req, res) => {
+app.post("/qr-async", async (req, res) => {
   try {
     let options = { type: "png", ...req.query };
-    const code = await qr.image(req.params.data, options);
-    let restype = getRespType(options.type);
+
+    let body = req.body;
+    console.log("Request Body: ", body);
+
+    const code = await qr.image(body.data, options);
+    let restype = getRespType(options);
     res.type(restype);
     code.pipe(res);
   } catch (err) {
     console.log(err.stack);
     res.status(500).send(err);
+  }
+});
+
+app.get("/qr-async/:data", async (req, res) => {
+  try {
+    let options = { type: "png", ...req.query };
+    const code = await qr.image(req.params.data, options);
+    let restype = getRespType(options);
+    res.type(restype);
+    code.pipe(res);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send(err);
+  }
+});
+
+app.post("/qr", (req, res) => {
+  /* Synchronous Mode */
+  try {
+    let options = { type: "png", ...req.query };
+
+    let body = req.body;
+
+    const code = qr.imageSync(body.data, options);
+    const filePath = path.join(
+      __dirname,
+      "images",
+      uuidv4() + "." + options.type
+    );
+    console.log(filePath);
+    fs.writeFileSync(filePath, code);
+
+    let restype = getRespType(options);
+    res.type(restype);
+
+    res.sendFile(filePath);
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).send(err);
   }
 });
 
@@ -46,7 +88,7 @@ app.get("/qr/:data", (req, res) => {
     console.log(filePath);
     fs.writeFileSync(filePath, code);
 
-    let restype = getRespType(options.type);
+    let restype = getRespType(options);
     res.type(restype);
 
     res.sendFile(filePath);
